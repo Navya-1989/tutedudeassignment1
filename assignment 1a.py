f8ff000153c484ea3c6b786ef99b93ac5dbb0c5d
Navya-1989 <navyabetha@gmail.com>
2025-04-25 14:42:19 +0530

    Add files via upload

---
+++ assignment 1a.py
@@ -0,0 +1,10 @@
+#problem 1
+a= input('Enter the first number: ')
+a= int(a)
+b= input('Enter the second number: ')
+b= int(b)
+print('addition:',a+b)
+print('subtraction:',a-b)
+print('multiplication:',a*b)
+print('division:',a/b)
+
